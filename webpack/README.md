a8f60897715bab97bf810d789340f9dcacc4189e
Vladimir Machacek <8machy@seznam.cz>
2021-12-18 17:56:30 +0100

    Add Webpack example

---
+++ webpack/README.md
@@ -0,0 +1,6 @@
+The Webpack commands are listed inside the package.json
+
+```
+yarn build
+yarn watch
+```
---
+++ webpack/webpack.config.js
@@ -0,0 +1,52 @@
+
+const { nativePreset } = require('@stylify/stylify');
+const { Bundler } = require('@stylify/bundler');
+const path = require('path');
+
+class StylifyPlugin {
+	apply(compiler) {
+		nativePreset.compiler.variables = {
+			blue: 'steelblue'
+		};
+
+		const bundler = new Bundler({
+			compilerConfig: nativePreset.compiler,
+			watchFiles: compiler.options.watch || false
+		});
+
+		bundler.bundle([
+			{
+				outputFile: './index.css',
+				files: ['./index.html']
+			}
+		]);
+
+		compiler.hooks.beforeRun.tapPromise(StylifyPlugin.name, () => {
+			return bundler.waitOnBundlesProcessed();
+		});
+		compiler.hooks.beforeRun.tapPromise(StylifyPlugin.name, () => {
+			return bundler.waitOnBundlesProcessed();
+		});
+	}
+}
+
+module.exports = {
+	entry: './input.js',
+	mode: 'production',
+	module: {
+		rules: [
+			{
+				test: /\.css$/i,
+				use: ["style-loader", "css-loader", "postcss-loader"]
+			},
+		],
+	},
+	plugins: [
+		new StylifyPlugin(),
+	],
+	output: {
+		path: path.resolve(__dirname),
+		filename: 'index.js',
+		libraryTarget: 'umd'
+	}
+};
