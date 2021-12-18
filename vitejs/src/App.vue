d41e1bfdbfb7293391959207cdd022c448450c83
Vladimir Machacek <8machy@seznam.cz>
2021-12-18 21:34:17 +0100

    Add Vite-Vue example

---
+++ vitejs/src/App.vue
@@ -0,0 +1,3 @@
+<template>
+  <div class="color:$blue font-size:48px">Hello World!</div>
+</template>
---
+++ vitejs/vite.config.js
@@ -0,0 +1,37 @@
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import { nativePreset } from '@stylify/stylify';
+import { Bundler } from '@stylify/bundler';
+
+// https://vitejs.dev/config/
+export default defineConfig(({ mode}) => {
+  nativePreset.compiler.variables = {
+    blue: 'steelblue'
+  };
+
+  const bundler = new Bundler({
+    compilerConfig: nativePreset.compiler,
+    watchFiles: mode === 'development'
+  });
+
+  const bundle = async () => {
+    bundler.bundle([
+      {
+        files: ['./src/**/*.vue'],
+        outputFile: './src/assets/stylify.css'
+      }
+    ]);
+    return bundler.waitOnBundlesProcessed();
+  };
+
+  const stylifyBundlerPlugin = () => {
+    return {
+      name: 'stylify',
+      options: bundle
+    }
+  };
+
+  return {
+    plugins: [vue(), stylifyBundlerPlugin()]
+  }
+})
