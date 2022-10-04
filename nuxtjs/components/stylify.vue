3042379048527c4bb30c9757418459a50a10705b
Vladimír <8machy@seznam.cz>
2022-10-04 14:00:42 +0200

    Next, Nuxt, Reactjs, Svelte examples are now on stackblitz

--- nuxtjs/components/stylify.vue
+++
@@ -1,3 +0,0 @@
-<template>
-	<p class="font-size:$paragraphFontSize">This is a Nuxt.js example. You can change it however you want :).</p>
-</template>
--- vitejs/src/App.vue
+++
@@ -1,3 +0,0 @@
-<template>
-  <div class="color:$blue font-size:48px">Hello World!</div>
-</template>
