5b3677e0704ca8fcabb6b34bc8732b84096e83cd
Vladimír <8machy@seznam.cz>
2022-10-17 17:34:32 +0200

    Update laravel example

--- laravel/resources/views/welcome.blade.php
+++ laravel/resources/views/welcome.blade.php
@@ -4,12 +4,18 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Stylify Laravel framework Example</title>
-    <link href="/css/homepage.css" rel="stylesheet">
+    <title>Stylify & Laravel Example</title>
+
+    @vite('resources/css/stylify.css')
 </head>
 
-<body class="antialiased">
-    <div class="color:$blue font-size:48px">Hello World!</div>
+<body>
+    <div class="
+        text-align:center
+        font-size:48px
+        font-family:arial
+        color:#ff2d20
+        margin-top:32vh
+    ">Stylify + Laravel = 🚀</div>
 </body>
-
 </html>
